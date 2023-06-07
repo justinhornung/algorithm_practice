ea628223a676e095bb6d0364bbe8aae7f04ca8fb
justinhornung <jhornung328i@gmail.com>
2023-06-07 18:45:25 -0400

    Initial commit

---
+++ less_than_100.rb
@@ -0,0 +1,17 @@
+# Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
+
+input = [99, 101, 88, 4, 2000, 50, -3]
+
+def less_than_one_hundred(numbers)
+  numbers_less_than_one_hundred = []
+  numbers.each { |num|
+    if num < 100
+      numbers_less_than_one_hundred << num
+    end
+  }
+  return numbers_less_than_one_hundred
+end
+
+puts less_than_one_hundred(input)
+
+# Output: [99, 88, 4, 50]
